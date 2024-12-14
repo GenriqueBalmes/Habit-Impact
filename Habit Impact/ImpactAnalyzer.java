afbb764efea8b68463ffc8dd493083010eb5b6e4
GenriqueBalmes <163899643+GenriqueBalmes@users.noreply.github.com>
2024-12-14 20:24:12 +0800

    Update ImpactAnalyzer.java

--- Habit Impact/ImpactAnalyzer.java
+++ Habit Impact/ImpactAnalyzer.java
@@ -1,15 +1,1 @@
-public class ImpactAnalyzer {
-    // Customizable impact multiplier based on habit type
-    public int calculateImpactScore(Habit habit) {
-        int multiplier = 10; // Default multiplier
 
-        // Customize multiplier based on habit type
-        if (habit instanceof ScreentimeHabit) {
-            multiplier = 5;  // Screen time might have a different multiplier
-        } else if (habit instanceof Food) {
-            multiplier = 8;  // Food habit might have a higher multiplier
-        }
-
-        return habit.getDailyScore() * multiplier; // Impact score is daily score multiplied by the habit's impact factor
-    }
-}
